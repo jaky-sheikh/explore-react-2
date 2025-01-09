8abd1b61232b0bea859956caf655d6042073b327
jaky-sheikh <jaky@eu4m.eu>
2025-01-09 12:25:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Counter from './Counter';
+import Team from './Team';
+
+function App() {
+
+  function handleClick() {
+    alert('button clicked');
+  }
+
+  return (
+    <>
+
+      <h3>React core concept part 2</h3>
+
+      {/* <Team></Team> */}
+      <Counter></Counter>
+
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => { alert('Clicked 2') }}>Click me 2</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Team.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react"
+
+// export default function Team() {
+
+//     const [team, setTeam] = useState(11);
+
+//     const teamStyle = {
+//         border: '2px solid purple',
+//         margin: '15px',
+//         padding: '15px',
+//         borderRadius: '15px'
+//     }
+//     return (
+//         <div style={teamStyle}>
+//             <h3>Players: {team}</h3>
+//         </div>
+//     )
+// }
